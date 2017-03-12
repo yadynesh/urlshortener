4d0c9c13d7d4bdd7db6faead279884e7ae056963
Yadynesh Desai <yadyneshdesai@gmail.com>
2017-03-12 22:55:02 +0530

    Querying database with shortcode and raising 404

--- shortener/views.py
+++ shortener/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import View
 from django.http import HttpResponse
+from .models import MagicUrl
 # Create your views here.
 def magic_redirect_view(request,shortcode = None, *args , **kwargs):
-	return HttpResponse("Yo this is MagicUrl")
+
+	qs  = get_object_or_404(MagicUrl, shortcode=shortcode)
+	return HttpResponse("Yo this is MagicUrl "+qs.url)
+	'''
+	obj_url = None
+	qs = MagicUrl.objects.filter(shortcode__iexact = shortcode.lower())
+	if qs.exists() and qs.count() == 1:
+		obj_url = qs.first().url
+	try:
+		qs = MagicUrl.objects.get(shortcode=shortcode)
+	except:
+		return HttpResponse("shortcode not found")
+
+	
+	'''
+	
 
 class MagicCBView(View):
 
