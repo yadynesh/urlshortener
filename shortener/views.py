caa0162ce433926ab7608dc2fd4bfa1646312c1c
Yadynesh Desai <yadyneshdesai@gmail.com>
2017-03-13 12:25:10 +0530

    Added forms.py to shortener and created a django form

--- shortener/views.py
+++ shortener/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render,get_object_or_404
 from django.views import View
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import MagicUrl
+from .forms import SubmitUrlForm
 # Create your views here.
 
 	
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
-		return render(request, 'shortener/home.html', {})
+		the_form  = SubmitUrlForm()
+		context = {
+			"title" : "MagicUrl.co",
+			"form" : the_form
+		}
+		return render(request, 'shortener/home.html', context)
+
+	def post(self, request, *args, **kwargs):
+		print (request.POST)
+		print (request.POST.get('url')) #url is the name of the input field in home.html
+		the_form  = SubmitUrlForm(request.POST)
+		if the_form.is_valid():
+			print(the_form.cleaned_data)
+
+		context = {
+			"title" : "MagicUrl.co",
+			"form" : the_form
+		}
+		return render(request, 'shortener/home.html', context)
 
 
 class MagicCBView(View):
@@ -16,8 +35,6 @@ class MagicCBView(View):
 		qs  = get_object_or_404(MagicUrl, shortcode=shortcode)
 		return HttpResponseRedirect(qs.url)
 		
-
-
 
 
 # def magic_redirect_view(request,shortcode = None, *args , **kwargs):
